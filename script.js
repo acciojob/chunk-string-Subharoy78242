56acae6c26acc51cea054dd863ed862908260188
Subharoy78242 <57645302+Subharoy78242@users.noreply.github.com>
2024-06-24 22:10:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function stringChop(str, size) {
-  if (str === null || size <= 0) {
-    return [];
-  }
-
-  if (str === '') {
+  if (str === null || str === '' || size <= 0) {
     return [];
   }
 
